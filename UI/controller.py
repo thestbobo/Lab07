b51716ca7e3b2ea63bb6c8ceb3793de59116f2c7
cmas1 <car.masone@gmail.com>
2024-04-20 08:45:10 +0200

    Aggiunta schema controller

--- UI/controller.py
+++ UI/controller.py
@@ -1,17 +1,26 @@
 import flet as ft
 
+from UI.view import View
+from model.model import Model
+
 
 class Controller:
-    def __init__(self, view, model):
+    def __init__(self, view: View, model: Model):
         # the view, with the graphical elements of the UI
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        # other attributes
+        self._mese = 0
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
-            return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
-        self._view.update_page()
+    def handle_umidita_media(self, e):
+        pass
+
+
+
+    def handle_sequenza(self, e):
+        pass
+
+    def read_mese(self, e):
+        self._mese = int(e.control.value)
+
